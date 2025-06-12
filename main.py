090eb196c5ee0e79342d815fa26954a1b23f7ae4
arnaud-brrt <arnaud.barrat@ensae.fr>
2025-06-12 12:51:05 +0000

    Add Events USD Price

--- main.py
+++ main.py
@@ -2,13 +2,11 @@
 
 from datetime import datetime
 from src.borrows_timespan.timespan_functions import all_first_last_repayments
-from src.borrows_timespan.event_funtion_extraction import collect_events_data
-from src.borrows_timespan.standardisation import (
-    standardisation_borrow,
-    standardisation_repay,
-    rescaling_borrows,
-    rescaling_repay,
+from src.borrows_timespan.data_extraction import (
+    collect_events_data,
+    collect_reserves_data,
 )
+from src.borrows_timespan.standardisation import rescaling_borrow, rescaling_repay
 
 borrow = "borrow"
 repay = "repay"
@@ -21,15 +19,11 @@ stop = datetime(2023, 2, 7)
 # Collect the data
 df_borrow = collect_events_data(event_type=borrow, start=start, stop=stop)
 df_repay = collect_events_data(event_type=repay, start=start, stop=stop)
+reserves = collect_reserves_data(start=start, stop=stop)
 
-# Rename the assets
-df_borrow = standardisation_borrow(df_borrow)
-df_repay = standardisation_repay(df_repay)
-
-# Rescale the amounts
-df_borrow = rescaling_borrows(df_borrow)
-df_repay = rescaling_repay(df_repay)
+# Rescale the df
+df_borrow = rescaling_borrow(df_borrow, reserves)
+df_repay = rescaling_repay(df_repay, reserves)
 
 # Create the dataframe
 df = all_first_last_repayments(df_borrow, df_repay)
-print(df)
