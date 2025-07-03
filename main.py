a60554cafce76d44296fa8580f5216a59d226fd7
arnaud-brrt <arnaud.barrat@ensae.fr>
2025-07-03 13:31:37 +0000

    Fichiers sur onyxia

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Main ETL
-
+import boto3
+import json
 from datetime import datetime
 from src.borrows_timespan.timespan_functions import (
     all_first_last_repayments,
@@ -14,6 +15,13 @@ from src.borrows_timespan.data_extraction import (
     extract_user_transactions,
 )
 
+# Inputs
+YOUR_ACCESS_KEY = "INPUT_YOUR_ACCESS_KEY"
+YOUR_SECRET_KEY = "INPUT_YOUR_SECRET_KEY"
+YOUR_TOKEN = "INPUT_YOUR_TOKEN"
+KEY = "INPUT_YOUR_ALCHEMY_API_KEY"
+
+
 # Define the time span
 # The first data available are on January 27th, 2023
 start = datetime(2023, 1, 27)
@@ -25,9 +33,24 @@ df_borrow, df_repay = collect_borrow_repay(start=start, stop=stop)
 # Create the dataframe
 df = all_first_last_repayments(df_borrow, df_repay)
 
+client_s3 = boto3.client(
+    "s3",
+    endpoint_url="https://" + "minio.lab.sspcloud.fr",
+    aws_access_key_id=f"{YOUR_ACCESS_KEY}",
+    aws_secret_access_key=f"{YOUR_SECRET_KEY}",
+    aws_session_token=f"{YOUR_TOKEN}",
+    verify=False,
+)
+
+data = client_s3.get_object(
+    Bucket="arnaudbrrt",
+    Key=f"/diffusion/aave_user_activity/blocksTimestamps.json",
+)["Body"].read()
+blocks_timestamps = json.loads(data)
+
 df = add_timestamp(
-    df,
-    blocksTimestamps="data/blocksTimestamps.json",
+    df=df,
+    blocksTimestamps=blocks_timestamps,
     col_blockNumber_borrow="Borrow_blockNumber",
     col_day_borrow="Borrow_Day",
     col_blockNumber_first="First_Repay_blockNumber",
@@ -36,12 +59,10 @@ df = add_timestamp(
     col_day_last="Last_Repay_Day",
 )
 
-key = "YOUR_ALCHEMY_API_KEY"
+df = extract_user_transactions(df=df, alchemy_api_key=KEY)
 
-df = extract_user_transactions(df=df, alchemy_api_key=key)
+df = classify_addresses(df=df, alchemy_api_key=KEY)
 
-df = classify_addresses(df=df, alchemy_api_key=key)
-
-df = fetch_erc20_metadata(df=df, alchemy_api_key=key)
+df = fetch_erc20_metadata(df=df, alchemy_api_key=KEY)
 
 df.to_csv("data/df_tx.csv", index=False)
